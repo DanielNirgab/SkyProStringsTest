156359aabe2ed39aa3812beb2b3cfe94608c8bee
DanielNirgab <bgrln347@gmail.com>
2022-10-22 16:42:44 +0300

    Task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+
 public class Main {
     public static void main(String[] args) {
+        String fullString = "Курсы по джава от SkyPro";
+        doSubString(fullString);
+    }
 
+    private static void doSubString(String fullString) {
+        doSubStringToChars(fullString);
+        System.out.println();
+        doSubStringToWords(fullString);
+    }
+
+    //
+    private static void doSubStringToWords(String fullString) {
+        String[] strings = fullString.split("[^A-zА-я0-9]");
+        for (String str :
+                strings) {
+            System.out.println(str);
+        }
+    }
+
+    private static void doSubStringToChars(String fullString) {
+        char[] strToChar = fullString.toCharArray();
+        for (char chars :
+                strToChar) {
+            if (chars != ' ') {
+                System.out.print(chars + " ");
+            }
+        }
     }
 }
